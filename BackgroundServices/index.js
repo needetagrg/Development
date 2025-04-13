7fbfca713d6f2d6414767b157c697703e52335de
needetagrg <needetagrg22@gmail.com>
2025-04-13 22:09:40 +0545

    Background services

--- BackgroundServices/index.js
+++ BackgroundServices/index.js
@@ -2,6 +2,9 @@ import express from "express";
 import dotenv from "dotenv";
 import dbconnection from "./utils/db.js";
 import cron from "node-cron";
+import sendWelcomeEmail from "./EmailServices/sendWelcomeEmail.js";
+import sendPendingOrderEmail from "./EmailServices/sendPendingOrderEmail.js";
+import sendDeliveredOrderEmail from "./EmailServices/sendDeliveredOrderEmail.js";
 
 dotenv.config();
 const app = express();
@@ -11,13 +14,15 @@ const PORT = process.env.PORT;
 //SCHEDULE SERVICES
 const services = () => {
   cron.schedule(" * * * * * * ", () => {
-    
+    sendWelcomeEmail();
+    sendPendingOrderEmail();
+    sendDeliveredOrderEmail();
   });
 };
 
 services();
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Backgroundservices is running on port ${PORT}`);
   dbconnection();
 });
--- BackgroundServices/models/user.model.js
+++ BackgroundServices/models/user.model.js
@@ -1,5 +1,4 @@
 import mongoose from "mongoose";
-import bcrypt from "bcryptjs";
 
 const userSchema = mongoose.Schema(
   {
@@ -35,17 +34,6 @@ const userSchema = mongoose.Schema(
   { timestamps: true }
 );
 
-userSchema.pre("save", async function (next) {
-  if (!this.isModified("password")) {
-    next();
-  }
-  const salt = await bcrypt.genSalt(10);
-  this.password = await bcrypt.hash(this.password, salt);
-});
-
-userSchema.methods.matchPassword = async function (enteredPasword) {
-  return await bcrypt.compare(enteredPasword, this.password);
-};
 
 const User = mongoose.model("User", userSchema);
 export default User;
